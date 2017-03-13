f82b4dbed06ff0b1f3b7cdab288e2d390915e6a6
decheng wang <wdc_sdau@163.com>
2017-03-14 00:05:49 +0800

    autoload router

--- core/autoLoad.php
+++ core/autoLoad.php
@@ -1,11 +1,22 @@
 <?php
 
+define("CLASSPOSTFIX", '.class.php');
+include COREPATH . DIRECTORY_SEPARATOR . Router.class.php
+
 spl_autoload_register('magicLoad');
 set_error_handler('magic\Exception::dealError');
 set_exception_handler('magic\Exception::dealException');
 register_shutdown_function('magic\Exception::dealShutdown');
 
+Router::dispatch();
+
 function magicLoad($class)
 {
-	
+	$file = $class . CLASSPOSTFIX;
+	if (is_file($file))
+	{
+		include $file
+	}
+	return
 }
+
--- core/router/Router.class.php
+++ core/router/Router.class.php
@@ -6,5 +6,11 @@ namespace magic
 
 Class Router
 {
-	
+	public static function dispatch()
+	{
+		$router = $_GET['r'];
+		list($controller, $method) = explode('/', $router);
+		$controllerName = ucfirst($controller) . 'Controller';
+		call_user_func(array(__NAMESPACE__ . '\$controllerName', $method));
+	}	
 }
